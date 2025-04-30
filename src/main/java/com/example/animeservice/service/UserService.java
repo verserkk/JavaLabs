442ffadc959870b323c641f7c4796c165860eb20
verserk <bountyhunter231@mail.ru>
2025-04-30 10:27:53 +0300

    lab5(bulk+tests/fix)

--- src/main/java/com/example/animeservice/service/UserService.java
+++ src/main/java/com/example/animeservice/service/UserService.java
@@ -93,7 +93,6 @@ public class UserService {
         }
         userRepository.deleteById(id);
 
-        // Invalidate cache entries related to this user
         cacheService.invalidate("user_" + id);
         cacheService.invalidateByPrefix("user_search_");
         cacheService.invalidateByPrefix("user_collections_");
